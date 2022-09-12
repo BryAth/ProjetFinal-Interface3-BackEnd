fec30f4149335d52dc18849e3c2bb9c34c8645b8
Bryan Lequeux <Lequeux.Bryan95@gmail.com>
2022-09-12 12:21:00 +0200

    Création du controller User

---
+++ controllers/user-controllers.js
@@ -0,0 +1,62 @@
+const User = require ('../models/user-models')
+
+const userMapper= user => new UserDTO(user.id,user.pseudo,user.firstname,user.lastname,user.email);
+
+
+
+const userController = {
+
+
+    getAll:async(req,res) => {
+        const users = await User.find()
+        res.status(200).json(users)
+    },
+
+
+
+
+    getById : async(req,res) => {
+        const id = req.params.id
+        const users = await User.findById(id);
+
+        if(!user) {
+            res.status(404).json('User non trouvé ! ')
+        }
+        res.status(200).json(user)
+    },
+
+
+
+    create : async (req,res) => {
+        const userToAdd = User(req.body);
+        await userToAdd.save()
+
+        if(!userToAdd){
+            res.status(404).json('Modèle non respecté')
+        }
+        res.status(200).json(userToAdd)
+    },
+
+
+
+    update : async(req,res) => {
+        const id = req.params.id;
+
+        const {pseudo,firstname,lastname,email} = req.body
+
+        const userUpdated = await User.findByIdAndUpdate(id,{pseudo,firstname,lastname,email},{returnDocument:'after'})
+
+        res.status(200).json(userUpdated)
+    },
+    delete : async (req,res) => {
+        const id = req.params.id;
+        const userToDelete = await User.findByIdAndDelete(id);
+        if(!userToDelete) {
+            res.sendStatus(404).json('ID non conforme ')
+        }
+        res.sendStatus(200).json('User bel et bien delete')
+    }
+}
+
+module.exports = userController
+
